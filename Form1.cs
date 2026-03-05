44d9832db13f85b2af90840ab4f25330ed510044
Oma10ar <omar19mousa@gmail.com>
2026-03-06 00:05:14 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testtttt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int Counter = 0;
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Counter++;
+            label1.Text = Counter.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            timer1.Enabled = true; 
+            button2.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled =false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            Counter = 0;
+            timer1.Enabled=true;
+        }
+    }
+}
